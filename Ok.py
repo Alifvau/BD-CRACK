7d5ceabec4caa2f14c19db9e6f5901daf08854f8
Alif Hasan <96680774+Alifvau@users.noreply.github.com>
2022-05-07 13:15:46 +0600

    Create Ok.py

---
+++ Ok.py
@@ -0,0 +1,13 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from km import login()
+    login()
+elif bit == '32bit':
+    print "Opps Your Device Not Supported"
